a140498e549116c7a77a5c2b0282a517e770b5f4
vaish567 <vaishnav.parte@gmail.com>
2024-08-03 13:56:20 +0530

    update-on-nextjs-web-app

---
+++ components/BlogList/BlogList.jsx
@@ -0,0 +1,68 @@
+import { blog_data } from "@/assets/assets";
+import React, { useState } from "react";
+import BlogItem from "../BlogItem/BlogItem";
+
+function BlogList() {
+  const [menu, setMenu] = useState("All");
+
+  return (
+    <div>
+      {" "}
+      <div className="flex justify-center gap-6 my-10">
+        <button
+          onClick={() => setMenu("All")}
+          className={
+            menu === "All" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
+          }
+        >
+          All
+        </button>
+        <button
+          onClick={() => setMenu("Technology")}
+          className={
+            menu === "Technology"
+              ? "bg-black text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+        >
+          Technology
+        </button>
+        <button
+          onClick={() => setMenu("Startup")}
+          className={
+            menu === "Startup" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
+          }
+        >
+          Startup
+        </button>
+        <button
+          onClick={() => setMenu("Lifestyle")}
+          className={
+            menu === "Lifestyle"
+              ? "bg-black text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+        >
+          Lifestyle
+        </button>
+      </div>
+      <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+        {blog_data
+          .filter((item) => (menu === "All" ? true : item.category === menu))
+          .map((item, index) => {
+            return (
+              <BlogItem
+                title={item.title}
+                image={item.image}
+                description={item.description}
+                category={item.category}
+                key={index}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default BlogList;
